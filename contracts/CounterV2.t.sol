135ac6b3e7cea1beb32836657bd1c40006db799d
heeronchang <heeronchang@outlook.com>
2025-11-26 16:26:58 +0800

    feat: integrate forge-std, add CounterV2 solidity file and it's test file CounterV2.t.sol who include base test functions name started test keyword and fuzz test name started with testFuzz

---
+++ contracts/CounterV2.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.28;
+
+import {CounterV2} from "./CounterV2.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract CounterV2Test is Test {
+  CounterV2 counter;
+
+  function setUp() public {
+    counter = new CounterV2();
+  }
+
+  function test_InitialValueIsZero() public view {
+    require(counter.x() == 0, "x should start at 0");
+  }
+
+  function test_IncIncreasesByOne() public {
+    counter.inc();
+    require(counter.x() == 1, "inc should increase x by 1");
+  }
+
+  function test_IncByIncreasesByGivenAmount() public {
+    counter.incBy(3);
+    require(counter.x() == 3, "incBy should increase x by the given amount");
+  }
+
+  function test_IncEmitsIncrementEvent() public {
+    vm.expectEmit();
+    emit CounterV2.Increment(1);
+
+    counter.inc();
+  }
+
+  function testFuzz_Inc(uint8 x) public {
+    for (uint8 i = 0; i < x; i++) {
+      counter.inc();
+    }
+
+    require(counter.x() == x, "Value after calling inc x times should be x");
+  }
+}
