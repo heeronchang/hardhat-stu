d610471cc2acecc053128e0c2086d7ea1c085229
heeronchang <heeronchang@outlook.com>
2025-11-26 16:29:27 +0800

    feat: add and config dependencies Viem, @nomicfoundation/hardhat-toolbox-viem and @nomicfoundation/hardhat-ignition,

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,10 @@
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+
 import { configVariable, defineConfig } from "hardhat/config";
 
 export default defineConfig({
-  plugins: [hardhatToolboxMochaEthersPlugin],
+  plugins: [hardhatToolboxMochaEthersPlugin, hardhatToolboxViemPlugin],
   solidity: {
     profiles: {
       default: {
